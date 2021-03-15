7638f3279dc94a999d6d6c683b9cfabe65d4da0f
bryamc1 <79555225+bryamc1@users.noreply.github.com>
2021-03-14 22:12:08 -0400

    Update Animate.js

--- AnimateNums/Animate.js
+++ AnimateNums/Animate.js
@@ -234,10 +234,11 @@ function start()  {
 
 function countNums(){
     var context = document.getElementById("area").getContext("2d");
+	context.fillRect(50,50,100,200);
     if(counter>9){
         context.drawImage(img,scene.frames[1].frame.x,scene.frames[1].frame.y,scene.frames[1].frame.w,scene.frames[1].frame.h,20,50,70,100);
         context.drawImage(img,scene.frames[0].frame.x,scene.frames[0].frame.y,scene.frames[0].frame.w,scene.frames[0].frame.h,90,50,90,100);
-		clearInterval(stop);
+	clearInterval(stop);
     }
     else {
 		context.drawImage(img,scene.frames[counter].frame.x,scene.frames[counter].frame.y,scene.frames[counter].frame.w,scene.frames[counter].frame.h,60,50,80,100);
