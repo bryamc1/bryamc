3807125a39b93aa5644a768445a4b92984b37183
bryamc1 <79555225+bryamc1@users.noreply.github.com>
2021-05-09 00:43:45 -0400

    Add files via upload

---
+++ audioproj/audio.js
@@ -0,0 +1,46 @@
+var song1 = document.getElementById('song1');
+var song2 = document.getElementById("song2");
+var song3 = document.getElementById("song3");
+var song4 = document.getElementById("song4");
+var song5 = document.getElementById("song5");
+
+function playFirstSong() {
+    song1.play();
+}
+
+function pauseFirstSong() {
+    song1.pause();
+}
+
+function playSecondSong() {
+    song2.play();
+}
+
+function pauseSecondSong() {
+    song2.pause();
+}
+
+function playThirdSong() {
+    song3.play();
+}
+
+function pauseThirdSong() {
+    song3.pause();
+}
+
+function playFourthSong() {
+    song4.play();
+}
+
+function pauseFourthSong() {
+    song4.pause();
+}
+
+function playFifthSong() {
+    song5.play();
+}
+
+function pauseFifthSong() {
+    song5.pause();
+}
+
